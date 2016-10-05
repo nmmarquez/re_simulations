a7539a4997f6cb62e37e0dd541daee582d068d07
Neal Marquez <nmmarquez@neal-MacBookPro.attlocal.net>
2016-10-05 15:43:41 -0700

    begin stepwise comparisons of gee vs melm

--- gee_test/gee_mm.R
+++ gee_test/gee_mm.R
@@ -1,8 +1,31 @@
 rm(list=ls())
 set.seed(123)
-source("./ar1.R")
 library(gee)
 library(lme4)
+library(mvtnorm)
+
+# function for generating an AR1 precision and or var-covar matrix
+ar1_matrix <- function(N, rho, sigma, vcov=FALSE){
+    mat <- matrix(data=0, nrow=N, ncol=N)
+    for(i in 2:N){
+        mat[i,i] <- (1. + rho**2) / sigma**2
+        mat[i-1,i] <- -rho / sigma**2
+        mat[i,i-1] <- -rho / sigma**2
+    }
+    for(i in c(1,N)){
+        mat[i,i] <- 1. / sigma**2
+    }
+    if(vcov){
+        mat <- solve(mat)
+    }
+    mat
+}
+
+# simulate an ar1 process
+sim_ar1 <- function(N, rho, sigma){
+    vcov_mat <- ar1_matrix(N, rho, sigma, vcov=TRUE)
+    as.vector(rmvnorm(1, mean=rep(0, N), sigma=vcov_mat))
+}
 
 # sim data with known residual structure 
 N <- 1000
@@ -25,10 +48,24 @@ df <- data.frame(y, x1, location=site_number_sample)
 df <- df[order(df$location),]
 
 # summaries of each model type
+
+run_model <- function(iterations=200, res="unstructured"){
+    sample_sizes <- seq(0, N , N/iterations)
+    var <- 
+    # make sure samples are greater than 20
+    sample_sizes <- sample_sizes[sample_sizes >= 20]
+    samples <- lapply(sample_sizes, function(x) sample(1:N, size=x))
+    m1 <- t(sapply(samples, function(x) lm(y ~ x1, data=df[x,])$coefficients))
+    m2 <- t(sapply(samples, function(x)
+        lmer(y ~ x1 + (1|location), data=df[x,])@beta))
+    m3 <- t(sapply(samples, function(x)
+        gee(y ~ x1, id=location, data=df[x,], corstr=res)$coefficients))
+    return(None)
+}
 pop_samp <- sample(1:N, site_number)
-summary(lm(y ~ x1, data=df[pop_samp,]))
-summary(lmer(y ~ x1 + (1|location), data=df[pop_samp,]))
-summary(gee(y ~ x1, id=location, data=df[pop_samp,]))
+m1 <- lm(y ~ x1)
+m2 <- lmer(y ~ x1 + (1|location), data=df)
+m3 <- gee(y ~ x1, id=location, data=df, corstr="unstructured")
 
 # now use the wrong covariance structure in the random effects model
 ar1_rho <- .99
