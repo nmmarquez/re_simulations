b76a840ede92c970cdd1e1e09e98f20da21f5914
Neal Marquez <nmmarquez@gmail.com>
2017-07-22 00:10:03 -0700

    launch models script

--- inla/launch_models.py
+++ inla/launch_models.py
@@ -7,15 +7,15 @@ def main():
     """
     Run all inla tmb comaprisons
     """
-    rhos = np.arange(.01, 1, .1)
-    sigmas = np.arange(.01, 2, .2)
-    ranges = np.arange(.01, 1, .1)
+    rhos = np.arange(.01, 1, .15)
+    sigmas = np.arange(.01, 2, .25)
+    ranges = np.arange(.01, 1, .15)
     Ns = np.array([50, 100, 500, 1000, 10000])
     file_dir = os.path.dirname(os.path.realpath(__file__))
     execf_ = os.path.join(file_dir, "compare_2D.R")
     qsub_template = (
             "qsub -b y -pe multi_slot 10 -now no -P proj_forecasting "
-            "-N {r}{s}{p}{n} {e} --rho {p} -N {n} --sigma {s} --range {r}"
+            "-N j{r}{s}{p}{n} {e} --rho {p} -N {n} --sigma {s} --range {r}"
             )
     for p in rhos:
         for s in sigmas:
