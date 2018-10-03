aedac19e7e50b7fc79106fe7a2565d00de227145
Neal Marquez <nmmarquez@protonmail.com>
2018-10-03 13:06:58 -0700

    update simulation plots

--- inla/theyrethesame.R
+++ inla/theyrethesame.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-pacman::p_load(INLA, ggplot2, data.table, lattice)
+pacman::p_load(INLA, ggplot2, data.table, lattice, ar.matrix)
 set.seed(124)
 # compare the INLA Q matrix vs the by hand to make sure we are on the same page
 
@@ -7,8 +7,10 @@ plot_mesh_sim <- function(x, proj){
     M <- length(proj$x)
     DT <- data.table(x=rep(proj$x, M), y=rep(proj$y, each=M), 
                      obs=c(inla.mesh.project(proj, field=x)))
-    ggplot(DT, aes(x, y, z= obs)) + geom_tile(aes(fill = obs)) + theme_bw() + 
-        lims(y=c(0,1), x=c(0,1)) + scale_fill_gradientn(colors=heat.colors(8))
+    ggplot(DT, aes(x, y, z= obs)) + geom_raster(aes(fill = obs)) +
+        theme_void() +
+        lims(y=c(0,1), x=c(0,1)) +
+        scale_fill_distiller(palette="Spectral")
 }
 
 n <- 1000
@@ -16,7 +18,7 @@ loc <- matrix(runif(n*2), n, 2)
 mesh <- inla.mesh.create(loc, refine=list(max.edge=0.05))
 plot(mesh)
 points(loc[,1], loc[,2], col="red", pch=20)
-proj <- inla.mesh.projector(mesh)
+proj <- inla.mesh.projector(mesh, dims=c(300,300), xlim=0:1, ylim=0:1)
 
 sigma0 <-  .5   ## Standard deviation
 range0 <- 1. ## Spatial range
@@ -29,7 +31,8 @@ spde <- inla.spde2.matern(mesh)
 Q1 <- inla.spde2.precision(spde, theta=c(log(tau0), log(kappa0)))
 Q2 <- tau0**2 * (kappa0**4 * spde$param.inla$M0 + 
                      2 * kappa0**2 *spde$param.inla$M1 + spde$param.inla$M2)
-x <- as.vector(inla.qsample(n=1, Q2))
+x <- as.vector(sim.AR(1, Q2))
+x <- x - mean(x)
 
 plot_mesh_sim(x, proj)
 cov1 <- rnorm(n)
@@ -47,6 +50,8 @@ summary(spatmodel)
 spatialhat <- inla.spde2.result(spatmodel, "id", spde)
 xhat <- spatialhat$summary.values["0.5quant"]
 plot_mesh_sim(c(xhat)[[1]], proj)
+plot_mesh_sim(spatialhat$summary.values$sd, proj) +
+    geom_point(aes(V1, V2, z=NULL, fill=NULL), data=as.data.frame(loc), size=.2)
 
 exp(spatmodel$summary.hyperpar$mean[2:3])
 c(tau0, kappa0)
--- ppp/ppp.R
+++ ppp/ppp.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-pacman::p_load(INLA, ggplot2, data.table, lattice, arm, dplyr, TMB)
+pacman::p_load(INLA, ggplot2, data.table, lattice, arm, dplyr, TMB, ar.matrix)
 set.seed(124)
 # compare the INLA Q matrix vs the by hand to make sure we are on the same page
 
@@ -19,7 +19,7 @@ points(loc[,1], loc[,2], col="red", pch=20)
 proj <- inla.mesh.projector(mesh, dims=c(500, 500))
 
 sigma0 <-  .2   ## Standard deviation
-range0 <- 1. ## Spatial range
+range0 <- .1 ## Spatial range
 kappa0 <- sqrt(8) / range0
 tau0 <- 1/(sqrt(4*pi)*kappa0*sigma0)
 spde <- inla.spde2.matern(mesh)
@@ -27,7 +27,7 @@ spde <- inla.spde2.matern(mesh)
 ##############
 Q2 <- tau0**2 * (kappa0**4 * spde$param.inla$M0 + 
                      2 * kappa0**2 *spde$param.inla$M1 + spde$param.inla$M2)
-x_ <- as.vector(inla.qsample(n=1, Q2))
+x_ <- as.vector(sim.AR(1, Q2))
 x <- x_ - mean(x_)
 
 mesh2DF(x) %>%
@@ -35,7 +35,7 @@ mesh2DF(x) %>%
     geom_raster(aes(fill = obs)) + 
     theme_void() + 
     lims(y=c(0,1), x=c(0,1)) + 
-    scale_fill_gradientn(colors=heat.colors(8))
+    scale_fill_distiller(palette = "Spectral")
 
 cov1 <- rnorm(n)
 
@@ -46,7 +46,10 @@ hist(p)
 hist(denom)
 
 DT <- data.table(
-    y=rbinom(n, denom, p), cov=cov1, id=1:n, denom=denom, geo=mesh$idx$loc-1)
+    y=rbinom(n, denom, p),
+    cov=cov1, id=1:n, denom=denom,
+    geo=mesh$idx$loc-1,
+    lon=loc[,1], lat=loc[,2])
 summary(DT)
 
 spatform <- y ~ cov1 + f(id, model=spde)
@@ -63,7 +66,7 @@ spatialhat <- inla.spde2.result(spatmodel, "id", spde)
 xhat <- spatialhat$summary.values$`0.5quant`
 bind_rows(
     mutate(mesh2DF(x), state="Observed"),
-    mutate(mesh2DF(xhat), state="Estimated TMB")) %>%
+    mutate(mesh2DF(xhat), state="Estimated INLA")) %>%
     ggplot(aes(x, y, z=obs)) +
     geom_raster(aes(fill = obs)) +
     theme_void() +
@@ -71,13 +74,20 @@ bind_rows(
     scale_fill_distiller(palette = "Spectral") +
     facet_wrap(~state)
 
+mesh2DF(spatialhat$summary.values$sd) %>%
+    ggplot(aes(x, y, z=obs)) +
+    geom_raster(aes(fill = obs)) +
+    theme_void() +
+    scale_fill_distiller(palette = "Spectral") +
+    geom_point(aes(x=lon, y=lat, z=NULL, fill=NULL), data=DT)
+
 exp(spatmodel$summary.hyperpar$mean[1:2])
 c(tau0, kappa0)
 
 # now run the TMB model using ./st.cpp
 setwd("~/Documents/re_simulations/ppp/")
 
-runModel <- function(DT, recompile=FALSE, symbolic=TRUE){
+runModel <- function(DT, recompile=FALSE, symbolic=TRUE, draws=1000){
     Data <- list(
         y=DT$y, x=DT$cov, geo=DT$geo, denom=DT$denom,
         M0=spde$param.inla$M0, M1=spde$param.inla$M1, M2=spde$param.inla$M2)
@@ -105,19 +115,36 @@ runModel <- function(DT, recompile=FALSE, symbolic=TRUE){
         objective=Obj$fn,
         gradient=Obj$gr,
         control=list(eval.max=1e4, iter.max=1e4))
+    sdrep <- sdreport(Obj, getJointPrecision=TRUE)
+    zindex <- "z" == row.names(sdrep$jointPrecision)
+    Qz <- sdrep$jointPrecision[zindex,zindex]
+    Zdraws <- sim.AR(draws, Qz)
     Report <- Obj$report()
-    list(obj=Obj, opt=Opt, report=Report) 
+    zDF <- tibble(
+        mu=Report$z,
+        sd=apply(Zdraws, 2, sd),
+        lwr=apply(Zdraws, 2, quantile, probs=.025),
+        upr=apply(Zdraws, 2, quantile, probs=.975)
+    )
+    list(obj=Obj, opt=Opt, sd=sdrep, z=zDF)
 }
 
 system.time(tmbModel <- runModel(DT))
-system.time(runModel(DT, symbolic=F))
 
 bind_rows(
     mutate(mesh2DF(x), state="Observed"),
-    mutate(mesh2DF(tmbModel$report$z), state="Estimated TMB")) %>%
+    mutate(mesh2DF(tmbModel$z$mu), state="Estimated TMB"),
+    mutate(mesh2DF(xhat), state="Estimated INLA")) %>%
     ggplot(aes(x, y, z=obs)) +
     geom_raster(aes(fill = obs)) +
     theme_void() +
     lims(y=c(0,1), x=c(0,1)) +
     scale_fill_distiller(palette = "Spectral") +
-    facet_wrap(~state)
+    facet_wrap(~state, nrow=2)
+
+mesh2DF(tmbModel$z$sd) %>%
+    ggplot(aes(x, y, z=obs)) +
+    geom_raster(aes(fill = obs)) +
+    theme_void() +
+    scale_fill_distiller(palette = "Spectral") +
+    geom_point(aes(x=lon, y=lat, z=NULL, fill=NULL), data=DT)
