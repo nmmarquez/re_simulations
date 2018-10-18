0bdc2c51d6609e20040592197d78bca3b6327374
nmarquez <nmarquez@uw.edu>
2018-10-18 08:40:43 -0700

    update data test

--- ppp/dataTest.R
+++ ppp/dataTest.R
@@ -4,20 +4,29 @@ library(sf)
 
 setwd("~/Documents/re_simulations/ppp/data/")
 
-# everything in here is a cbh
-allDF <- read_rds("./prepared_cbh_2018_09_04_11_49_25.RDS")
-head(allDF)
+if(!file.exists("DRdata.csv")){
+    # everything in here is a cbh
+    allDF <- read_rds("./prepared_cbh_2018_09_04_11_49_50.RDS")
+    head(allDF)
+    
+    # looking at this group the DR("DOM") looks like a good candidate
+    allDF %>%
+        mutate(has_coords=is.na(longnum)) %>%
+        group_by(country, has_coords) %>%
+        summarize(n=n()) %>%
+        filter(n() == 2)
+    
+    DF <- filter(allDF, country == "DOM") %>%
+        as_tibble %>%
+        mutate(GAUL_CODE=as.numeric(location_code)) %>%
+        rename(Nid=nid) %>%
+        left_join(select(read_csv("./id.csv"), Nid, Title), by="Nid") %>%
+        select(-Nid)
+    
+    write_csv(DF, "DRdata.csv")
+}
 
-# looking at this group dominican republic("DOM") looks like a good candidate
-allDF %>%
-    mutate(has_coords=is.na(longnum)) %>%
-    group_by(country, has_coords) %>%
-    summarize(n=n()) %>%
-    filter(n() == 2)
-
-DF <- filter(allDF, country == "DOM") %>%
-    as_tibble %>%
-    mutate(GAUL_CODE=as.numeric(location_code))
+DF <- read_csv("DRdata.csv", col_types="ccdcidcddccddidcdc")
 
 cat(paste0("Number of Observations: ", sum(DF$N), "\n"))
 cat(paste0("Number of Deaths: ", round(sum(DF$died)), "\n"))
@@ -31,24 +40,18 @@ admin1Codes <- DF %>%
     unname
 
 # row counts
-table(DF$shapefile)
+table(DF$shapefile, useNA="ifany")
 
 # weighted counts
 DF %>%
     group_by(shapefile) %>%
     summarize(personCount=sum(N))
 
+# temporal breakdown
 DF %>%
-    select(nid, shapefile) %>%
-    rename(Nid=nid) %>%
-    unique %>%
-    left_join(read_csv("./id.csv"), by="Nid") %>%
-    select(Title, shapefile)
-
-# temporal breakdown: I definitely dont get this
-DF %>%
-    group_by(shapefile, year, svyyr, source) %>%
-    summarize(personCount=sum(N))
+    group_by(Title, year) %>%
+    summarize(personCount=sum(N)) %>%
+    as.data.frame
 
 shapeADMIN <- st_read("admin2013_1.shp")
 shapeADMINDR <- shapeADMIN[shapeADMIN$GAUL_CODE %in% admin1Codes,]
@@ -79,27 +82,25 @@ pointSFDF <- DF %>%
         crs = "+proj=longlat +datum=WGS84 +no_defs") %>%
     st_combine
 
-# Beautiful!
-ggplot(DF, aes(x=longnum, y=latnum)) +
-    geom_point() + 
-    theme_classic()
-
 # No :(
 ggplot(pointSFDF) +
     geom_sf(data=shapeADMINDR) +
     geom_sf(size=.2, alpha=.3) +
-    theme_classic()
+    theme_classic() +
+    ggtitle("Dominican Republic Gelocated Survey Data")
 
 do.call(rbind, dataCountList) %>%
     ggplot(aes(fill=obsCount)) +
     geom_sf() +
     scale_fill_distiller(palette="Spectral") +
     theme_classic() +
-    facet_wrap(~set, nrow=2)
+    facet_wrap(~set, nrow=2) +
+    ggtitle("Dominican Republic Areal Survey Data")
 
-do.call(rbind, dataCountList[1:3]) %>%
+do.call(rbind, dataCountList[1:2]) %>%
     ggplot(aes(fill=obsCount)) +
     geom_sf() +
     scale_fill_distiller(palette="Spectral") +
     theme_classic() +
-    facet_wrap(~set, nrow=2)
+    facet_wrap(~set, nrow=2) +
+    ggtitle("Dominican Republic Areal Survey Data")
