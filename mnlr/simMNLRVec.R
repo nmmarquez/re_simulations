a9419cf42cb0d62c884e11ca274859044b0fbd44
nmarquez <nmarquez@uw.edu>
2018-11-06 11:02:00 -0800

    refactor and comment mnlrvec

--- mnlr/simMNLRVec.R
+++ mnlr/simMNLRVec.R
@@ -42,16 +42,20 @@ print(all.equal(rowSums(y_obs), rep(1, N)))
 ratio2Pred <- function(X, model, draws=NULL){
     G <- length(model$par) / ncol(X) + 1
     if(!is.null(draws)){
+        # recursive process for simulations
         betas_ <- rmvnorm(draws, model$par, solve(model$hessian))
         y_prob <- array(0, dim=c(nrow(X), G, draws))
         for(d in 1:draws){
-            m <- list(par=betas_[d,])
-            y_prob[,,d] <- ratio2Pred(X, m) 
+            m <- list(par=betas_[d,]) # treat the sims like true values
+            y_prob[,,d] <- ratio2Pred(X, m) # rerun the function with sim
         }
     }
     else{
+        # transform vector of beta paramters into matrix for ease of use
         betas_ <- matrix(model$par, ncol=G-1, nrow=ncol(X))
+        # get the ratios for the groups vs ref
         ratio_ref <- exp(X %*% betas_)
+        # the response var is transformed to probability space
         p1 <- apply(ratio_ref, 1, function(x) 1 / (1 + sum(x)))
         y_prob <- cbind(p1, ratio_ref * p1)
     }
@@ -60,29 +64,19 @@ ratio2Pred <- function(X, model, draws=NULL){
 
 # get the estimates
 optimMultiVec <- function(params, X, Y){
-    # transform vector of beta paramters into matrix for ease of use
-    betas_ <- matrix(params, ncol=ncol(Y)-1, nrow=ncol(X))
-    # get the ratios for the groups vs ref
-    ratio_ref <- exp(X %*% betas_)
-
-    # the response var is transformed to probability space
-    p1 <- apply(ratio_ref, 1, function(x) 1 / (1 + sum(x)))
-    y_prob <- cbind(p1, ratio_ref * p1)
-    
     # apply the log likelihood to the observations
-    nll <- -sum(log(y_prob) * Y)
-    nll
+    -sum(log(ratio2Pred(X, list(par=params))) * Y)
 }
 
 fitMultiVec <- function(X, Y){
     # problematic if not all groups are well defined
     # probably can get around this with sampling
     y <- apply(y_obs, 1, function(x) which(x == max(x))[1])
-    
+
     # get starting values from regular ass multinom
     initDF <- as.data.frame(cbind(y=y, X))
     pstart <- c(coef(multinom(y ~ . + 0, data=initDF, trace=FALSE)))
-    
+
     # optimizeeeee
     optim(pstart, optimMultiVec, X=X, Y=Y, hessian=TRUE, method="BFGS")
 }
@@ -93,6 +87,7 @@ testFit <- fitMultiVec(covs, y_obs)
 testFit$convergence # make sure we get 0 exit status
 # visually inspect betas not bad!
 betas
+matrix(testFit$par, nrow = M+1)
 
 # we can also get draws of probability predictions
 dim(ratio2Pred(covs, testFit, draws = 1000))
