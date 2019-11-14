1281c4b4595a54a982e06b6b3653bc2b40f36850
Neal Marquez <nmarquez@uw.edu>
2019-11-14 09:03:47 -0800

    update preston

---
+++ preston/plots.R
@@ -0,0 +1,118 @@
+rm(list=ls())
+library(tidyverse)
+library(gapminder)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(sf)
+# extract world data
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+gapminder %>% 
+    ggplot(aes(gdpPercap, lifeExp, color=year)) +
+    geom_point() +
+    theme_classic() +
+    scale_color_distiller(palette = "Spectral") +
+    scale_x_log10() +
+    labs(x="GDP Per Capita", y="Life Expectancy", color="Year")
+
+gapminder %>% 
+    filter(year == 2007) %>% 
+    ggplot(aes(gdpPercap, lifeExp)) +
+    geom_point(aes(color=continent)) +
+    theme_classic() +
+    scale_x_log10() +
+    labs(x="GDP Per Capita", y="Life Expectancy", color="Continent") +
+    geom_smooth(method="lm", se=F, color = "black", linetype = 3)
+
+slopeDF <- t(sapply(sort(unique(gapminder$year)), function(y){
+    lm(lifeExp ~ log(gdpPercap), data = filter(gapminder, year == y)) %>%
+        summary() %>%
+        {c(.$coefficients[2,], rsq=.$r.squared)}})) %>%
+    as_tibble() %>%
+    mutate(lwr = Estimate - 1.98 * `Std. Error`) %>%
+    mutate(upr = Estimate + 1.98 * `Std. Error`) %>%
+    mutate(year = sort(unique(gapminder$year)))
+
+slopeDF %>%
+    ggplot(aes(year, Estimate, ymin = lwr, ymax = upr, color = year)) +
+    geom_point() +
+    geom_errorbar() +
+    scale_color_viridis_c() +
+    theme_classic() +
+    labs(x = "Year", y = "Slope of Preston Curve") +
+    ggtitle("Declining Effect of Wealth on Life Expectancy")
+
+gapminder %>%
+    group_by(year) %>%
+    summarize(
+        `Life Expectancy (sd)` = sd(lifeExp),
+        `Log GDP (sd)` = sd(log(gdpPercap))) %>%
+    pivot_longer(-year) %>%
+    ggplot(aes(x = year, y = value)) +
+    geom_line() +
+    facet_wrap(~name, scales = "free_y") +
+    theme_classic() +
+    labs(x = "Year", y = "Satndard Deviation") +
+    ggtitle("Changes in Spread of Preston Curve Components")
+
+gapminder %>%
+    filter(year == 2007 | year == 1957) %>%
+    arrange(country, year) %>%
+    group_by(continent, country) %>%
+    summarize(deltaLEX = diff(lifeExp), deltalGDP = diff(log(gdpPercap))) %>%
+    {lm(deltaLEX ~ deltalGDP, data = .)} %>%
+    summary()
+
+deltaDF <- gapminder %>%
+    filter(year == 2007 | year == 1957) %>%
+    arrange(country, year) %>%
+    group_by(continent, country) %>%
+    summarize(deltaLEX = diff(lifeExp), deltalGDP = diff(log(gdpPercap)))
+
+deltaDF %>%
+    ggplot(aes(x = deltalGDP, y = deltaLEX)) +
+    geom_point() +
+    theme_classic() +
+    geom_smooth(method="lm") +
+    geom_hline(yintercept = 0, linetype = 2, color = "red") +
+    geom_vline(xintercept = 0, linetype = 2, color = "red") +
+    geom_label(
+        aes(x = x, y = y, label = tx),
+        fill = "#4b2e83", color = "white", fontface = "bold",
+        data = tibble(x = -1, y = 30, tx = "Decrease GDP\nIncrease LEX")) +
+    geom_label(
+        aes(x = x, y = y, label = tx),
+        fill = "#b7a57a", color = "white", fontface = "bold",
+        data = tibble(x = -1, y = -5, tx = "Decrease GDP\nDecrease LEX")) +
+    geom_label(
+        aes(x = x, y = y, label = tx),
+        fill = "#000000", color = "white", fontface = "bold",
+        data = tibble(x = 3, y = -5, tx = "Increase GDP\nDecrease LEX")) +
+    geom_label(
+        aes(x = x, y = y, label = tx),
+        fill = "#DCDCDC", color = "white", fontface = "bold",
+        data = tibble(x = 3, y = 30, tx = "Increase GDP\nIncrease LEX")) +
+    labs(y = "Change in Life Expectancy", x = "Change in Log GDP")
+
+deltaDF %>%
+    mutate(Delta = case_when(
+        deltaLEX >= 0 & deltalGDP >= 0 ~ "Increase GDP\nIncrease LEX",
+        deltaLEX < 0 & deltalGDP < 0 ~ "Decrease GDP\nDecrease LEX",
+        deltaLEX >= 0 & deltalGDP < 0 ~ "Decrease GDP\nIncrease LEX",
+        deltaLEX < 0 & deltalGDP >= 0 ~ "Increase GDP\nDecrease LEX",
+        TRUE ~ NA_character_)) %>%
+    mutate(geounit = as.character(country)) %>%
+    mutate(geounit = ifelse(
+        geounit == "United States", "United States of America", geounit)) %>%
+    mutate(geounit = ifelse(
+        geounit == "Korea, Dem. Rep.", "South Korea", geounit)) %>%
+    mutate(geounit = ifelse(
+        geounit == "Congo, Dem. Rep.", "Democratic Republic of the Congo", geounit)) %>%
+    mutate(geounit = ifelse(
+        geounit == "Congo, Rep.", "Republic of the Congo", geounit)) %>%
+    {right_join(world, ., by="geounit")} %>%
+    # filter(geounit != "Antarctica") %>%
+    ggplot() +
+    geom_sf(aes(fill=Delta)) +
+    theme_void() +
+    scale_fill_manual(values=c("#b7a57a", "#4b2e83", "#000000", "#DCDCDC"))
